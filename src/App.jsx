3d4228122c41d0d8b7643e4edeab52f208343df8
Brandon Chau <brandon.chau578@gmail.com>
2024-10-21 18:11:59 -0700

    feat added: API integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [list, setList] = useState(null)
+  const API_KEY = 'f7e2bd77e3623095181ecc8d44f8107edc03db42b30d763907dc35ec586d38cd';
+  
+  useEffect(()=>{
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" + API_KEY
+      );
+      const json = await response.json();
+      setList(json);
+    };
+    fetchAllCoinData().catch(console.error);
+  },[]);
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className = "whole-page">
+      <h1> My Crypto List</h1>
+        <ul>
+          {list && Object.entries(list.Data).map(([coin]) =>
+            list.Data[coin].PlatformType === "blockchain" ? (
+              <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+            ) : null
+          )}
+        </ul>
+    </div>
+  );
+
 }
 
-export default App
+export default App;
